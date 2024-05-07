f5dc5eea428c2fb2dbdad3d8e4f2a477d695187e
Daniel Artiaga Perez <dartiagaperez.cf@iesesteveterradas.cat>
2024-05-07 19:07:36 +0200

    Daniel 1
    planetas actualizado

--- src/Planet.java
+++ src/Planet.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+
 public class Planet {
 	int technologyDefense;
 	int technologyAtack;
@@ -8,4 +9,45 @@ public class Planet {
 	int upgradeDefenseTechnologyDeuteriumCost;
 	int upgradeAttackTechnologyDeuteriumCost;
 	ArrayList<MilitaryUnit>[] army = new ArrayList[7];
+	
+	public void upgradeTechnologyDefense() {
+		
+	}
+	
+
+	public void upgradeTechnologyAttack() {
+		
+	}
+	
+	public void newLightHunter(int n) {
+		
+	}
+	
+	public void newHeavyHunter(int n) {
+		
+	}
+	
+	public void newBattleShip(int n) {
+		
+	}
+	
+	public void newArmoredShip(int n) {
+		
+	}
+	
+	public void newMissileLauncher(int n) {
+		
+	}
+	
+	public void newIonCannon(int n) {
+		
+	}
+	
+	public void newPlasmaCannon(int n) {
+		
+	}
+	
+	public void printStats() {
+		
+	}
 }
