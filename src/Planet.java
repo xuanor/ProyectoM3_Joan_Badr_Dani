21a89affc6f40720ddd317ddc300b693a6b9ca40
Daniel Artiaga Perez <dartiagaperez.cf@iesesteveterradas.cat>
2024-05-07 19:10:51 +0200

    Daniel 2
    Defensas actualizadas

--- src/Planet.java
+++ src/Planet.java
@@ -10,10 +10,81 @@ public class Planet {
 	int upgradeAttackTechnologyDeuteriumCost;
 	ArrayList<MilitaryUnit>[] army = new ArrayList[7];
 	
+	// Getters and setters
+	public int getTechnologyDefense() {
+		return technologyDefense;
+	}
+
+	public void setTechnologyDefense(int technologyDefense) {
+		this.technologyDefense = technologyDefense;
+	}
+
+	public int getTechnologyAtack() {
+		return technologyAtack;
+	}
+
+	public void setTechnologyAtack(int technologyAtack) {
+		this.technologyAtack = technologyAtack;
+	}
+
+	public int getMetal() {
+		return metal;
+	}
+
+	public void setMetal(int metal) {
+		this.metal = metal;
+	}
+
+	public int getDeuterium() {
+		return deuterium;
+	}
+
+	public void setDeuterium(int deuterium) {
+		this.deuterium = deuterium;
+	}
+
+	public int getUpgradeDefenseTechnologyDeuteriumCost() {
+		return upgradeDefenseTechnologyDeuteriumCost;
+	}
+
+	public void setUpgradeDefenseTechnologyDeuteriumCost(int upgradeDefenseTechnologyDeuteriumCost) {
+		this.upgradeDefenseTechnologyDeuteriumCost = upgradeDefenseTechnologyDeuteriumCost;
+	}
+
+	public int getUpgradeAttackTechnologyDeuteriumCost() {
+		return upgradeAttackTechnologyDeuteriumCost;
+	}
+
+	public void setUpgradeAttackTechnologyDeuteriumCost(int upgradeAttackTechnologyDeuteriumCost) {
+		this.upgradeAttackTechnologyDeuteriumCost = upgradeAttackTechnologyDeuteriumCost;
+	}
+
+	public ArrayList<MilitaryUnit>[] getArmy() {
+		return army;
+	}
+
+	public void setArmy(ArrayList<MilitaryUnit>[] army) {
+		this.army = army;
+	}
+	
+	// Constructor
+	public Planet(int technologyDefense, int technologyAtack, int metal, int deuterium,
+			int upgradeDefenseTechnologyDeuteriumCost, int upgradeAttackTechnologyDeuteriumCost,
+			ArrayList<MilitaryUnit>[] army) {
+		super();
+		this.technologyDefense = technologyDefense;
+		this.technologyAtack = technologyAtack;
+		this.metal = metal;
+		this.deuterium = deuterium;
+		this.upgradeDefenseTechnologyDeuteriumCost = upgradeDefenseTechnologyDeuteriumCost;
+		this.upgradeAttackTechnologyDeuteriumCost = upgradeAttackTechnologyDeuteriumCost;
+		this.army = army;
+	}	
+	
+	// Mas metodos
 	public void upgradeTechnologyDefense() {
 		
 	}
-	
 
 	public void upgradeTechnologyAttack() {
 		
@@ -49,5 +120,6 @@ public class Planet {
 	
 	public void printStats() {
 		
-	}
+	}	
+	
 }
