64fc160c59dd4df4da424ce83bce5b5dcd48b60e
Daniel Artiaga Perez <dartiagaperez.cf@iesesteveterradas.cat>
2024-05-07 18:24:48 +0200

    ASD

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
-		WAHRSVF`
-		avgdfo`vp
+		System.out.println("asdasdasdasdasdas");
+
 	}
 
 }
