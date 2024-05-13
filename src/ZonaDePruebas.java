6a9b3a889c61fb72d705d4ef03aa4ede3b917ed9
Daniel Artiaga Perez <dartiagaperez.cf@iesesteveterradas.cat>
2024-05-13 21:11:36 +0200

    Daniel 16

--- src/ZonaDePruebas.java
+++ src/ZonaDePruebas.java
@@ -1,44 +1,51 @@
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ZonaDePruebas {
 
 	public static void main(String[] args) {
-		
-//		int metal = 8000;
-//		int deuterium = 12500;
-//		int LightHunter = 8;
-//		int HeavyHunter = 9;
-//		int BattleShip = 10;
-//		int ArmoredShip = 11;
-	
 
-//		String ordenado = String.format("|%-11s | %4s| %8s | %7s", metal, deuterium, LightHunter, HeavyHunter);
-//		
-//		System.out.println("Metal = " + metal + "\n" + "Deuterium = " + deuterium + "\n" + "FLEET" +
-//		"\n" + "LightHunter = " + LightHunter + "\n" + "HeavyHunter = " + HeavyHunter);
-//	}
+		String urlDatos = "jdbc:oracle:thin:@192.168.56.101:1521/orcl";
+		String usuario = "ogame";
+		String pass = "ogame";
 		
-//        String ordenado = String.format(
-//        						   " MATERIALS \n\n"
-//        						 + " Metal%19d \n"
-//        						 + " Deuterium%15d \n\n"
-//        						 + " FLEET \n\n"
-//        						 + " LightHunter%13d \n"
-//        						 + " HeavyHunter%13d \n"
-//        						 + " BattleShip%14d \n"
-//        						 + " ArmoredShip%13d", 
-//                metal, deuterium, LightHunter, HeavyHunter, BattleShip, ArmoredShip);
-//		
-//		 Imprime el resultado del String.format
-//		System.out.println(ordenado);
-	
-	
-//	for (int i = 0; i < army.length; i++) {
-//	     System.out.println(army[i]);
-//	}
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			Connection conn = DriverManager.getConnection(urlDatos, usuario, pass);
+			
+			String query = "SELECT * FROM user_credentials";
+
+			
+			Statement stmnt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			
+			ResultSet rs = stmnt.executeQuery(query);
+			
+			while(rs.next()) {
+			System.out.println("ID = " + rs.getInt(1) + ", Password = " + rs.getString(2) + ", Name = " + rs.getString(3));
+			}
+			
+			
+            // Preparar la inserción de un nuevo registro
+//            String insertQuery = "INSERT INTO user_credentials (user_id, user_password, user_name) VALUES (?, ?, ?)";
+//            PreparedStatement insertStatement = conn.prepareStatement(insertQuery);
+//            insertStatement.setInt(1, 65);
+//            insertStatement.setString(2, "Contra1");
+//            insertStatement.setString(3, "danielin");
+//            insertStatement.executeUpdate();
+            
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("Driver no cargado");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Conexion no realizada");
+			e.printStackTrace();
+		}
 		
+	}
 }
-}
-
-
-
